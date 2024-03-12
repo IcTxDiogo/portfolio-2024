4d21449a0f27f90d1b244e50fc959ced132e4768
Santos <IcTxDiogo@gmail.com>
2024-03-11 22:57:42 -0400

    feat: add vitest

---
+++ src/__tests__/page.test.tsx
@@ -0,0 +1,13 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Home from "@/app/page";
+
+describe("Home", () => {
+  it("should render header", () => {
+    render(<Home />);
+
+    const header = screen.getByText(/Hello world/i);
+
+    expect(header).toBeDefined();
+  });
+});

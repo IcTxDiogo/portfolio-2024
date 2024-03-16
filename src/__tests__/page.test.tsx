b4c2b34962991a3982ab876e4d854ed55cba0c25
Santos <IcTxDiogo@gmail.com>
2024-03-16 10:53:44 -0400

    feat: move pages to (pages) to better organization

--- src/__tests__/page.test.tsx
+++ src/__tests__/page.test.tsx
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import Home from "@/app/page";
+import Home from "@/app/(pages)/page";
 
 describe("Home", () => {
   it("should render header", () => {

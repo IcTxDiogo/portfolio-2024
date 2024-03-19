d532a21590bfe12bb7baf54c9e421ab7d82eb7ca
Santos <IcTxDiogo@gmail.com>
2024-03-19 00:04:04 -0400

    feat: add home nav component

---
+++ src/__tests__/Home/HomeNavBar.test.tsx
@@ -0,0 +1,29 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import HomeNavBar from "@/components/pages/Home/HomeNavBar";
+
+describe("HomeNavBar", () => {
+  it("should render the logo", () => {
+    render(<HomeNavBar />);
+
+    const logo = screen.getByText(/Logo/i);
+
+    expect(logo).toBeDefined();
+  });
+
+  it("should render 3 links", () => {
+    render(<HomeNavBar />);
+
+    const links = screen.getAllByRole("link");
+
+    expect(links).toHaveLength(3);
+  });
+
+  it("should render the theme mode toggle", () => {
+    render(<HomeNavBar />);
+
+    const themeModeToggle = screen.getByText(/Toggle theme/i);
+
+    expect(themeModeToggle).toBeDefined();
+  });
+});
---
+++ src/components/pages/Home/HomeNavBar.tsx
@@ -0,0 +1,39 @@
+import { randomUUID, UUID } from "node:crypto";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { ThemeModeToggle } from "@/components/ThemeModeToggle";
+
+type HomeItem = {
+  id: UUID;
+  name: string;
+  link: string;
+};
+
+const homeItens: HomeItem[] = [
+  { id: randomUUID(), name: "About", link: "" },
+  { id: randomUUID(), name: "Projects", link: "" },
+  { id: randomUUID(), name: "Contact", link: "" },
+];
+
+export default function HomeNavBar() {
+  return (
+    <>
+      <nav className={"flex items-center justify-between p-5 shadow-md"}>
+        <section>Logo</section>
+        <ul className={"flex"}>
+          {homeItens.map((item) => (
+            <li key={item.id}>
+              <Link href={item.link}>
+                <Button variant={"link"}>{item.name}</Button>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <section>
+          <ThemeModeToggle />
+        </section>
+      </nav>
+    </>
+  );
+}
